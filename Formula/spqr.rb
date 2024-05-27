7336a8d3beaa3fc31ed7e3796074a1988af73e7f
Galileo Luna <39316537+galileoluna@users.noreply.github.com>
2024-05-27 11:43:58 -0300

    Update spqr.rb

--- Formula/spqr.rb
+++ Formula/spqr.rb
@@ -1,7 +1,7 @@
 class Spqr < Formula
     desc "Project for creating Go projects using hexagonal architecture"
     homepage "https://github.com/spqr-go/spqr"
-    url "https://github.com/spqr-go/spqr/releases/download/spqr-v1/spqr"
+    url "https://github.com/spqr-go/spqr/releases/download/spqr-v2/spqr"
     version "0.1.1"
     sha256 "f02ec499cc27e23899f0361625fdb3ee4820a6d057adad30413f406102acf1f0"
     license "MIT"
